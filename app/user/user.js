5b74decb7a5fdfc2cf125e635298f4ecdf14d779
Jordan Kirkman <jordyakirkman@gmail.com>
2015-02-20 16:29:29 -0700

    now returning user and binding it to the view

--- app/user/user.js
+++ app/user/user.js
@@ -27,7 +27,7 @@ angular.module('myApp.user', ['ngRoute', 'ngResource'])
 		// this callback will be called asynchronously
 		// when the response is available
 		console.log(data);
-		$scope.model = data;
+		$scope.model = data.items[0];
 	}).error(function(data, status, headers, config) {
 		// called asynchronously if an error occurs
 		// or server returns response with an error status.
