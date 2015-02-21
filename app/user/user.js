a4f72750a62428581fd83edd0610c8d3ef7b6f21
Jordan Kirkman <jordyakirkman@gmail.com>
2015-02-20 21:03:45 -0700

    added timeline data to user, added repeat for timeline in user template

--- app/user/user.js
+++ app/user/user.js
@@ -31,11 +31,32 @@ angular.module('myApp.user', ['ngRoute', 'ngResource'])
 			// when the response is available
 			$rootScope.user = data.items[0];
 			$scope.model = data.items[0];
+			$scope.getTimeline($scope.model);
 		}).error(function(data, status, headers, config) {
 			// called asynchronously if an error occurs
 			// or server returns response with an error status.
 			console.log('error ' + data.error_message);
 			$scope.error = data;
+		});
+	}
+
+	$scope.getTimeline = function(user){
+		$http({
+			url: 'https://api.stackexchange.com/2.2/me/timeline',
+			method: "GET",
+			params: {
+				access_token: $rootScope.access_token,
+				key: '6S9zu7acV8JdHBn473Q6yw((',
+				site: 'stackoverflow'
+			}
+		}).success(function(data, status, headers, config) {
+			// this callback will be called asynchronously
+			// when the response is available
+			user.timeline = data.items;
+		}).error(function(data, status, headers, config) {
+			// called asynchronously if an error occurs
+			// or server returns response with an error status.
+			console.log('error ' + data.error_message);
 		});
 	}
 
