6696bfb89d844aeaa3939b7fc368b97bced4d38b
Jordan Kirkman <jordyakirkman@gmail.com>
2015-02-21 23:48:14 -0700

    refactored question controller and added question service

--- app/question/question.js
+++ app/question/question.js
@@ -9,32 +9,41 @@ angular.module('app.question', ['ngRoute', 'ngResource'])
   });
 }])
 
-.controller('QuestionController', ['$scope', '$routeParams', '$rootScope', '$http', '$sce', function($scope, $routeParams, $rootScope, $http, $sce) {
+// get a question by id. the method returns a list, but since we will only submit one, we just want the first back
+.factory('Question', ['$resource', '$rootScope',
+	function($resource, $rootScope){
+		return $resource('https://api.stackexchange.com/2.2/questions/' + $routeParams.question_id, {}, {
+			query: {
+				method:'GET',
+				params:{
+					access_token: $rootScope.access_token,
+					key: '6S9zu7acV8JdHBn473Q6yw((',
+					filter: "!tRhd)msKfDI_kdNs2zdw2HVvoAIWUBj",
+					site: 'stackoverflow'
+				},
+				isArray:false,
+				// stackoverflow returns objects in an "items" array
+				// with the "me" method, we only need the first item in the response
+				transformResponse: function(data){
+					if(JSON.parse(data).items){
+						var question = JSON.parse(data).items[0];
+						// we need angular to allow these strings to be read as html
+						question.body = $sce.trustAsHtml(question.body);
+						question.answers.forEach(function(answer){
+							answer.body = $sce.trustAsHtml(answer.body);
+						});
+					} else {
+						return JSON.parse(data);
+					}
+				}
+			}
+		});
+	}
+])
+
+.controller('QuestionController', ['Question', '$scope', '$routeParams', '$rootScope', '$http', '$sce', function(Question, $scope, $routeParams, $rootScope, $http, $sce) {
 
 	// fetch the the current user and set it as the model
-	$http({
-		url: 'https://api.stackexchange.com/2.2/questions/' + $routeParams.question_id,
-		method: "GET",
-		params: {
-			access_token: $rootScope.access_token,
-			key: '6S9zu7acV8JdHBn473Q6yw((',
-			filter: "!tRhd)msKfDI_kdNs2zdw2HVvoAIWUBj",
-			site: 'stackoverflow'
-		}
-	}).success(function(data, status, headers, config) {
-		// convert the body strings to html
-		data.items[0].body = $sce.trustAsHtml(data.items[0].body);
-		if(data.items[0].answers){
-			data.items[0].answers.forEach(function(answer){
-				answer.body = $sce.trustAsHtml(answer.body);
-			});
-		}
-		$scope.model = data.items[0];
-	}).error(function(data, status, headers, config) {
-		// called asynchronously if an error occurs
-		// or server returns response with an error status.
-		console.log('error ' + data.error_message);
-		$scope.error = data;
-	});
+	$scope.model = Question.query();
 
 }]);
