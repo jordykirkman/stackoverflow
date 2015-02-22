dff1a0e0fc996c70548147636cb5599392901d09
Jordan Kirkman <jordyakirkman@gmail.com>
2015-02-22 16:46:00 -0700

    added favorites service and added to template in user

--- app/user/user.js
+++ app/user/user.js
@@ -25,7 +25,9 @@ angular.module('app.user', ['ngRoute', 'ngResource'])
 				// with the "me" method, we only need the first item in the response
 				transformResponse: function(data){
 					if(JSON.parse(data).items){
-						return JSON.parse(data).items[0];
+						var user = JSON.parse(data).items[0];
+						user.profile_image = user.profile_image.split('?')[0];
+						return user;
 					} else {
 						return JSON.parse(data);
 					}
@@ -110,8 +112,33 @@ angular.module('app.user', ['ngRoute', 'ngResource'])
 	}
 ])
 
-.controller('UserController', ['User', 'Timeline', 'Badges', 'Tags', '$scope', '$routeParams', '$rootScope', '$http', '$location',
-	function(User, Timeline, Badges, Tags, $scope, $routeParams, $rootScope, $http, $location) {
+// get the timeline of the currently logged in user
+.factory('Favorites', ['$resource', '$rootScope',
+	function($resource, $rootScope){
+		return $resource('https://api.stackexchange.com/2.2/me/favorites', {}, {
+			query: {
+				method:'GET',
+				params:{
+					access_token: $rootScope.access_token,
+					key: '6S9zu7acV8JdHBn473Q6yw((',
+					filter: '!--OzR77qYOnX',
+					site: 'stackoverflow'
+				},
+				isArray:true,
+				transformResponse: function(data){
+					if(JSON.parse(data).items){
+						return JSON.parse(data).items;
+					} else {
+						return JSON.parse(data);
+					}
+				}
+			}
+		});
+	}
+])
+
+.controller('UserController', ['User', 'Timeline', 'Badges', 'Tags', 'Favorites', '$scope', '$routeParams', '$rootScope', '$http', '$location',
+	function(User, Timeline, Badges, Tags, Favorites, $scope, $routeParams, $rootScope, $http, $location) {
 
 	// clear the hash from the oath
 	$location.hash('');
@@ -120,5 +147,6 @@ angular.module('app.user', ['ngRoute', 'ngResource'])
 	$scope.timeline = Timeline.query();
 	$scope.badges = Badges.query();
 	$scope.tags = Tags.query();
+	$scope.favorites = Favorites.query();
 
 }]);
