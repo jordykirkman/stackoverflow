3de2127ed63b0ac1298f8aee5ebd9cd7d86eed53
Jordan Kirkman <jordyakirkman@gmail.com>
2015-02-21 23:32:43 -0700

    refactored user controller to have 2 services that fetch data

--- app/user/user.js
+++ app/user/user.js
@@ -1,63 +1,71 @@
 'use strict';
 
-angular.module('myApp.user', ['ngRoute', 'ngResource'])
+angular.module('app.user', ['ngRoute', 'ngResource'])
 
 .config(['$routeProvider', function($routeProvider, $routeParams) {
   $routeProvider.when('/user/', {
     templateUrl: 'user/user.html',
-    controller: 'UserCtrl'
+    controller: 'UserController'
   });
 }])
 
-.controller('UserCtrl', ['$scope', '$routeParams', '$rootScope', '$http', '$location', function($scope, $routeParams, $rootScope, $http, $location) {
+// get the currently logged in user via the /me method
+.factory('User', ['$resource', '$rootScope',
+	function($resource, $rootScope){
+		return $resource('https://api.stackexchange.com/2.2/me', {}, {
+			query: {
+				method:'GET',
+				params:{
+					access_token: $rootScope.access_token,
+					key: '6S9zu7acV8JdHBn473Q6yw((',
+					site: 'stackoverflow'
+				},
+				isArray:false,
+				// stackoverflow returns objects in an "items" array
+				// with the "me" method, we only need the first item in the response
+				transformResponse: function(data){
+					if(JSON.parse(data).items){
+						return JSON.parse(data).items[0];
+					} else {
+						return JSON.parse(data);
+					}
+				}
+			}
+		});
+	}
+])
+
+// get the timeline of the currently logged in user
+.factory('Timeline', ['$resource', '$rootScope',
+	function($resource, $rootScope){
+		return $resource('https://api.stackexchange.com/2.2/me/timeline', {}, {
+			query: {
+				method:'GET',
+				params:{
+					access_token: $rootScope.access_token,
+					key: '6S9zu7acV8JdHBn473Q6yw((',
+					site: 'stackoverflow'
+				},
+				isArray:true,
+				transformResponse: function(data){
+					if(JSON.parse(data).items){
+						return JSON.parse(data).items;
+					} else {
+						return JSON.parse(data);
+					}
+				}
+			}
+		});
+	}
+])
+
+.controller('UserController', ['User', 'Timeline', '$scope', '$routeParams', '$rootScope', '$http', '$location',
+	function(User, Timeline, $scope, $routeParams, $rootScope, $http, $location) {
 
 	// clear the hash from the oath
 	$location.hash('');
 
-	if($rootScope.user){
-		$scope.model = $rootScope.user;
-	} else {
-		// fetch the the current user and set it as the model
-		$http({
-			url: 'https://api.stackexchange.com/2.2/me',
-			method: "GET",
-			params: {
-				access_token: $rootScope.access_token,
-				key: '6S9zu7acV8JdHBn473Q6yw((',
-				site: 'stackoverflow'
-			}
-		}).success(function(data, status, headers, config) {
-			// this callback will be called asynchronously
-			// when the response is available
-			$rootScope.user = data.items[0];
-			$scope.model = data.items[0];
-			$scope.getTimeline($scope.model);
-		}).error(function(data, status, headers, config) {
-			// called asynchronously if an error occurs
-			// or server returns response with an error status.
-			console.log('error ' + data.error_message);
-			$scope.error = data;
-		});
-	}
-
-	$scope.getTimeline = function(user){
-		$http({
-			url: 'https://api.stackexchange.com/2.2/me/timeline',
-			method: "GET",
-			params: {
-				access_token: $rootScope.access_token,
-				key: '6S9zu7acV8JdHBn473Q6yw((',
-				site: 'stackoverflow'
-			}
-		}).success(function(data, status, headers, config) {
-			// this callback will be called asynchronously
-			// when the response is available
-			user.timeline = data.items;
-		}).error(function(data, status, headers, config) {
-			// called asynchronously if an error occurs
-			// or server returns response with an error status.
-			console.log('error ' + data.error_message);
-		});
-	}
+	$scope.model = User.query();
+	$scope.timeline = Timeline.query();
 
 }]);
